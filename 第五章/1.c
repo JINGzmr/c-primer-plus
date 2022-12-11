03fad1636b133cf8d09d1b668fd9af7f1be33cc6
JINGzmr <2743377630@qq.com>
2022-12-11 22:42:05 +0800

    5

--- 第五章/1.c
+++ 第五章/1.c
@@ -1,7 +1,13 @@
 #include<stdio.h>
+#define t 60
 int main()
 {
-
+    int x;
+    scanf("%d",&x);
+    while(x>0){
+        printf("%d小时 %d分钟",x/t,x%t);
+        scanf("%d",&x);
+    }
 
 
     return 0;
