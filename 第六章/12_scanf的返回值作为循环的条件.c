92bc8b8a3a26e99de63e185fa032f67e151c319d
JINGzmr <2743377630@qq.com>
2022-12-12 10:46:47 +0800

    6

--- 第六章/12_scanf的返回值作为循环的条件.c
+++ 第六章/12_scanf的返回值作为循环的条件.c
@@ -5,28 +5,19 @@ int main(void)
     printf("Please enter a number (<= 0 to quit): ");
     while ((scanf("%d", &n) == 1) && (n > 0))
     {
-        double res1 = 0.0;
-        double res2 = 0.0;
-        for (i = 1; i <= n; i++)
-        {
-            res1 += 1.0 / i;
-            if (i % 2 == 1)
-            {
-                res2 += 1.0 / i;
-            }
-            else
-            {
-                res2 -= 1.0 / i;
-            }
+        double sum1 = 0.0;
+        double sum2 = 0.0;
+        
+        for (i = 1; i <= n; i++){
+            sum1 += 1.0 / i;
+            if (i % 2 == 1)sum2 += 1.0 / i;
+            else  sum2 -= 1.0 / i;
         }
-        printf("1.0 + 1.0/2.0 + 1.0/3.0 + 1.0/4.0 + ...");
-        printf("sum are %g\n", res1);
-        printf("1.0 - 1.0/2.0 + 1.0/3.0 - 1.0/4.0 + ...");
-        printf("sum are %g\n", res2);
-        printf("The sum of the first %d items of the two sequences is %g\n", n, res1 + res2);
-        printf("\nYou can enter again (<= 0 to quit): ");
+
+        printf("1.0 + 1.0/2.0 + 1.0/3.0 + 1.0/4.0 + ...= %g\n", sum1);
+        printf("1.0 - 1.0/2.0 + 1.0/3.0 - 1.0/4.0 + ...= %g\n", sum2);
+        printf("The sum of the first %d items of the two sequences is %g\n", n, sum1 + sum2);
     }
-    printf("Done.\n");
 
     return 0;
 }
